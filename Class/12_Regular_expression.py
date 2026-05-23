5b4c160bfaeb7aa8cae534def105d960e8af2c58
Pranjal <Pranjal.Verma@businessnext.com>
2026-05-23 12:53:12 +0530

    Add Reg_ex file

---
+++ Class/12_Regular_expression.py
@@ -0,0 +1,5 @@
+# Pattern matching:
+
+# 1.'and|org': This pattern matches either 'and' or 'org'. The '|' symbol is used to specify an alternative, so the regex will ....
+# ... match any string that contains either 'and' or 'org'.
+
